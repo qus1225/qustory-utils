23717a4969ef7fd01fa5d52ea5e58bbbf02d3b0b
Qus <qus1225@gmail.com>
2020-02-22 19:36:59 +0900

    Disqus  컴포넌트 추가

---
+++ src/components/Disqus.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Disqus } from 'gatsby-plugin-disqus';
+import styled from 'styled-components';
+import { Theme, THEME_LABEL } from '../types';
+
+const StyledDiv = styled.div`
+  padding-top: 50px;
+`;
+
+export interface IProps {
+  path: string;
+  identifier: string;
+  title: string;
+  theme: Theme;
+}
+
+const Container = (props: IProps) => {
+  const disqusConfig = {
+    url: `https://${THEME_LABEL[theme].EN.toLowerCase()}.qustory.com${
+      props.path
+    }`,
+    identifier: props.identifier,
+    title: props.title
+  };
+  return (
+    <StyledDiv>
+      <Disqus config={disqusConfig} />
+    </StyledDiv>
+  );
+};
+
+export default Container;
